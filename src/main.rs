16df31dee9b4866c521d7648b01f0cc47e416de8
franzageek <franzageek2020@gmail.com>
2025-06-03 22:03:01 +0200

    Add test code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 mod piece;
 mod board;
+mod coord;
 
 fn main() {
     println!("dama rust rewrite, v0.1");
     let board: board::Board = board::Board::init();
+    let moves: Option<Vec<u8>> = piece::possible_moves(&board.pieces[11], &board.tiles);
+    match moves {
+        Some(vec) => {
+            for i in vec {
+                println!("possible move: {i}");
+            }
+        }
+        _ => {}
+    }
     println!("Thanks for playing dama ;)");
 }
