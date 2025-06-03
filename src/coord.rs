4726a35751e9b0abde291c090025c78cd56296a8
franzageek <franzageek2020@gmail.com>
2025-06-03 21:57:01 +0200

    Fix conversion system

--- src/coord.rs
+++ src/coord.rs
@@ -1,15 +1,18 @@
 pub fn xy_from_n(n: u8) -> (u8, u8) {
-    let y: u8 = n - (n % 4) - if ( 
-        if n == 0 {
-            1
+    let y: u8 = (
+        n - (n % 4) - if ( 
+            if n == 0 {
+                1
+            } else {
+                n % 4
+            } 
+        ) == 0 {
+            4
         } else {
-            n 
-        } % 4
-    ) == 0 {
-        4
-    } else {
-        0
-    } / 4;
+            0
+        }
+    ) / 4;
+    
 
     let x: u8 = (
         if n % 4 == 0 {
