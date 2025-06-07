e64859bf1f17dff6420de89fe6e7f36e5377adaa
franzageek <franzageek2020@gmail.com>
2025-06-07 18:15:32 +0200

    Update test code

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,22 @@
-mod piece;
 mod board;
-mod coord;
-mod tiles;
 mod capture;
-mod ui;
+mod coord;
 mod game;
+mod piece;
+mod tiles;
+mod ui;
 
 use raylib;
 
 fn main() {
     println!("dama rust rewrite, v0.1");
     let mut board: board::Board = board::Board::init();
-    board.pieces[14].king = true;
     game::main_loop(
         raylib::init()
-        .size(
-            ui::WINDOW_SIZE as i32, 
-            ui::WINDOW_SIZE as i32
-        ).title("dama v0.1")
-        .build(),
-        &mut board
+            .size(ui::WINDOW_SIZE as i32, ui::WINDOW_SIZE as i32)
+            .title("dama v0.1")
+            .build(),
+        &mut board,
     );
-    /*match piece::from_n(23, &mut board.clone()) {
-        Some(piece) => {
-            let mut moves: Vec<u8> = piece::possible_moves(piece, &board.tiles);
-            for i in moves {
-                println!("possible move from 23: {i}");
-            }
-            piece::move_to(piece, 20, &mut board);
-            moves = piece::possible_moves(piece, &board.tiles);
-            match moves {
-                Some(vec) => {
-                    for i in vec {
-                        println!("possible move from 20: {i}");
-                    }
-                }
-                _ => { 
-                    println!("no move avaliable");
-                }
-            }
-            if let Some(piece2) = piece::from_n(12, &mut board.clone()) {
-                moves = piece::possible_moves(piece2, &board.tiles);
-                match moves {
-                    Some(vec) => {
-                        for i in vec {
-                            println!("possible move from 12: {i}");
-                        }
-                    }
-                    _ => { 
-                        println!("no move avaliable");
-                    }
-                }
-                piece::move_to(piece2, 16, &mut board);
-                board.tiles[29] = 0;
-                println!("{:?}", capture::get_possible(&piece2, &mut board, 0, tiles::Pos::None));
-            }
-        }
-        _ => { }
-    }*/
-    
     println!("Thanks for playing dama ;)");
 }
